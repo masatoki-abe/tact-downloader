9f19dc04b2d9114b2b7ce8d7180c15ee912a1c0f
masatoki-abe <masa.abe.0530@gmail.com>
2026-08-16 00:41:55 +0900

    test: classifier fixtureを匿名化

--- tact_downloader/__init__.py
+++ tact_downloader/__init__.py
@@ -8,8 +8,6 @@ load_dotenv(Path(__file__).parent.parent / ".env", override=True)
 TACT_BASE_URL = os.environ.get("TACT_BASE_URL", "https://tact.ac.thers.ac.jp").rstrip(
     "/"
 )
-VAULT_ROOT = os.environ.get(
-    "VAULT_ROOT", str(Path.home() / "document" / "obsidian-vault")
-)
+VAULT_ROOT = os.environ.get("VAULT_ROOT", str(Path.home() / "obsidian-vault"))
 DOWNLOAD_BASE = os.environ.get("DOWNLOAD_BASE", "大学")
 COOKIE_FILE = str(Path.home() / ".tact_cookies.json")
